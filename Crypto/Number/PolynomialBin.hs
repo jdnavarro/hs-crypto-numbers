b15d6b8b52ec2a181d3d4d021b6c23eeba860e81
Danny Navarro <j@dannynavarro.net>
2013-06-02 12:00:32 +0200

    Proper instances of `Show`, `Eq` and `Ord`

--- Crypto/Number/PolynomialBin.hs
+++ Crypto/Number/PolynomialBin.hs
@@ -10,14 +10,17 @@ module Crypto.Number.PolynomialBin
     ) where
 
 import Data.Bits (xor)
-import Data.List (sort, elemIndices)
+import Data.List (sort, elemIndices, intercalate)
 import Numeric (readInt, showIntAtBase)
 import Data.Vector (Vector)
 import qualified Data.Vector as V
 import Crypto.Number.Polynomial (Monomial(..), Polynomial, mulPoly)
 import qualified Crypto.Number.Polynomial as P
 
-newtype PolynomialBin = PolynomialBin (Vector Int) deriving (Show)
+newtype PolynomialBin = PolynomialBin (Vector Int) deriving (Eq,Ord)
+
+instance Show PolynomialBin where
+    show (PolynomialBin v) = intercalate "+" $ map (("x^" ++) . show) $ V.toList v
 
 toList :: PolynomialBin -> [Int]
 toList (PolynomialBin v) = V.toList v
@@ -61,4 +64,3 @@ readBin = fst . head . readInt 2 (`elem` "01") (\c -> if c == '1' then 1 else 0)
 
 showBin :: Int -> String
 showBin n = showIntAtBase 2 (\x -> if x == 0 then '0' else '1') n ""
-
