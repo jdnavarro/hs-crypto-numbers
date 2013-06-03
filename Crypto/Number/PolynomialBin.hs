dea9f3f8c6900ae376b38352eb219a99322442e7
Danny Navarro <j@dannynavarro.net>
2013-06-03 16:26:30 +0200

    Simplify `showBin`

--- Crypto/Number/PolynomialBin.hs
+++ Crypto/Number/PolynomialBin.hs
@@ -15,6 +15,7 @@ module Crypto.Number.PolynomialBin
 
 import Data.Bits (xor)
 import Data.List (sort, elemIndices, intercalate)
+import Data.Char (intToDigit)
 import Numeric (readInt, showIntAtBase)
 import Data.Vector (Vector)
 import qualified Data.Vector as V
@@ -69,7 +70,7 @@ readBin :: String -> Integer
 readBin = fst . head . readInt 2 (`elem` "01") (\c -> if c == '1' then 1 else 0)
 
 showBin :: Integer -> String
-showBin n = showIntAtBase 2 (\x -> if x == 0 then '0' else '1') n ""
+showBin = flip (showIntAtBase 2 intToDigit) []
 
 -----
 
